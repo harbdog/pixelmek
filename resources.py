dfa7e92aa3fda2397b5854cf59ab29d4b7ccf3a9
Eric H <harbdog@gmail.com>
2016-08-14 08:49:15 -0400

    Moved sounds to their own folder instead of inside data folder

--- resources.py
+++ resources.py
@@ -16,18 +16,18 @@ class Resources(object):
         Resources.flash03 = pyglet.resource.image('images/weapons/flash_03.png')
 
         # Preload sound effects
-        Resources.cannon_sound = Sound(pyglet.resource.file("data/sounds/autocannon-shot.ogg"))
-        Resources.explosion_sound = Sound(pyglet.resource.file("data/sounds/explosion-single.ogg"))
-        Resources.flamer_sound = Sound(pyglet.resource.file("data/sounds/flamer-shot.ogg"))
-        Resources.gauss_sound = Sound(pyglet.resource.file("data/sounds/gauss-shot.ogg"))
-        Resources.las_sound = Sound(pyglet.resource.file("data/sounds/laser-blast-long.ogg"))
-        Resources.machinegun_sound = Sound(pyglet.resource.file("data/sounds/machine-gun.ogg"))
-        Resources.ppc_sound = Sound(pyglet.resource.file("data/sounds/ppc-shot.ogg"))
+        Resources.cannon_sound = Sound(pyglet.resource.file("sounds/autocannon-shot.ogg"))
+        Resources.explosion_sound = Sound(pyglet.resource.file("sounds/explosion-single.ogg"))
+        Resources.flamer_sound = Sound(pyglet.resource.file("sounds/flamer-shot.ogg"))
+        Resources.gauss_sound = Sound(pyglet.resource.file("sounds/gauss-shot.ogg"))
+        Resources.las_sound = Sound(pyglet.resource.file("sounds/laser-blast-long.ogg"))
+        Resources.machinegun_sound = Sound(pyglet.resource.file("sounds/machine-gun.ogg"))
+        Resources.ppc_sound = Sound(pyglet.resource.file("sounds/ppc-shot.ogg"))
 
         Resources.missile_sounds = []
         for i in range(8):
-            Resources.missile_sounds.append(Sound(pyglet.resource.file("data/sounds/missile-shot-%s.ogg" % i)))
+            Resources.missile_sounds.append(Sound(pyglet.resource.file("sounds/missile-shot-%s.ogg" % i)))
 
         Resources.stomp_sounds = []
         for i in range(4):
-            Resources.stomp_sounds.append(Sound(pyglet.resource.file("data/sounds/mech-stomp-%s.ogg" % i)))
+            Resources.stomp_sounds.append(Sound(pyglet.resource.file("sounds/mech-stomp-%s.ogg" % i)))
