13d55659e24016b67f49c34ef33ec5ab192c96c9
Eric Harbeston <harbdog@gmail.com>
2016-11-20 17:55:18 -0500

    Updated bot to be able to move and then fire automatically

--- pixelmek/ai/bot.py
+++ pixelmek/ai/bot.py
@@ -1,5 +1,6 @@
 import random
 
+from cocos.actions import *
 from pixelmek.model.battle import Battle
 from pixelmek.model.battle import Player
 from pixelmek.ui import actions
@@ -18,23 +19,48 @@ class Bot(Player):
         board = Board.BOARD
 
         turn_unit = battle.getTurnUnit()
-        turn_player = turn_unit.getPlayer()
-
         curr_col, curr_row = turn_unit.getPosition()
 
-        move_col = curr_col + random.randint(-1, 1)
-        move_row = curr_row + random.randint(-1, 1)
-
-        while not battle.isCellAvailable(move_col, move_row):
-            move_col = curr_col + random.randint(-1, 1)
-            move_row = curr_row + random.randint(-1, 1)
-
-        turn_unit.move -= 1
+        turn_player = turn_unit.getPlayer()
 
         Interface.UI.setUnitStatsIndicatorsVisible(False)
 
         for cell in board.cellMap.itervalues():
             cell.remove_indicators()
+
+        # first, determine where to move
+        # for now, just find a spot that is both in range to some target and has fewest LOS to other targets
+        cells_in_range = battle.getCellsInRange(turn_unit.col, turn_unit.row, turn_unit.move)
+        enemy_units = battle.getEnemyUnits(turn_unit)
+
+        cell_pos_best = 0, 0
+        cell_los_best = 1000
+
+        for cell_pos in cells_in_range:
+            # cell_range = cells_in_range[cell_pos]
+            if not battle.isCellAvailable(*cell_pos):
+                continue
+
+            num_los = 0
+
+            for enemy in enemy_units:
+                if enemy.isDestroyed():
+                    continue
+
+                target_coords = enemy.getPosition()
+                if battle.hasTargetLOS(cell_pos, target_coords):
+                    num_los += 1
+
+            if num_los > 0 and num_los < cell_los_best:
+                cell_pos_best = cell_pos
+                cell_los_best = num_los
+
+        move_col, move_row = curr_col, curr_row
+        move_amt = 0
+        if cell_pos_best is not None:
+            move_col = cell_pos_best[0]
+            move_row = cell_pos_best[1]
+            move_amt = cells_in_range[move_col, move_row]
 
         animate_reverse = (turn_unit.col - move_col > 0) or (turn_unit.row - move_row > 0)
 
@@ -47,11 +73,43 @@ class Bot(Player):
 
             board.scroller.set_focus(*turn_cell_pos)
 
-            print('next turn')
-            actions.nextTurn()
+            # print('next turn')
+            # actions.nextTurn()
+            self._prepare_attack()
 
         turn_unit.sprite.strut(reverse=animate_reverse)
         turn_unit.sprite.moveToCell(move_col, move_row, animate_reverse, _ready_next_move)
 
+        turn_unit.move -= move_amt
         turn_unit.col = move_col
         turn_unit.row = move_row
+
+    def _prepare_attack(self):
+        battle = Battle.BATTLE
+        board = Board.BOARD
+
+        turn_unit = battle.getTurnUnit()
+
+        target_unit = None
+        target_to_hit = 0
+
+        enemy_units = battle.getEnemyUnits(turn_unit)
+        for enemy in enemy_units:
+            if enemy.isDestroyed():
+                continue
+
+            to_hit = battle.getToHit(turn_unit, enemy)
+
+            if to_hit > 0 and to_hit > target_to_hit:
+                target_unit = enemy
+                target_to_hit = to_hit
+
+        if target_unit is not None:
+            attack_time = actions.performAttackOnUnit(board, target_unit)
+
+            def _ready_next_turn():
+                Interface.UI.setUnitStatsIndicatorsVisible(True)
+                actions.nextTurn()
+
+            # start the next turn when the attack is completed
+            board.do(Delay(attack_time) + CallFunc(_ready_next_turn))
